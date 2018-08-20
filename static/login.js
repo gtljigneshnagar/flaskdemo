57e8167f5814d6ccd25318a64fb1ac66b06159f4
gtljigneshnagar <42536119+gtljigneshnagar@users.noreply.github.com>
2018-08-20 17:26:05 +0530

    Add files via upload

---
+++ static/login.js
@@ -0,0 +1,20 @@
+﻿$('login_btn').on('click', function () {
+    $.ajax({
+        url: '/login',
+        data: $('form').serialize(),
+        type: 'POST',
+        datatype : 'json',
+        success: function (data, textStatus) {
+            if (data.redirect){
+                window.location.href = data.redirect;
+            }
+            else {
+                $("#form").replaceWith(data.form);
+            }
+            console.log(response);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+});
---
+++ static/signup.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function() {
+    $('form').on('submit', function() {
+        var url = "{{ url_for('signup') }}";
+        $.ajax({
+            url: url,
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function (data) {
+                //$(location).attr('href', '/index')
+                console.log(response);
+            },
+            error : function (error) {
+                console.log(error);
+            }
+        });
+    });
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", "{{ form.csrf_token._value() }}")
+            }
+        }
+    })
+});
